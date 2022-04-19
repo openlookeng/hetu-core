b4cf65dc4a70ac148bdcad81508c6c77f504175c
shalumariamsheji <shalumariamsheji@gmail.com>
2022-04-19 13:50:30 +0530

    Resolving review comments

--- presto-tests/src/main/java/io/prestosql/tests/QueryAssertions.java
+++ presto-tests/src/main/java/io/prestosql/tests/QueryAssertions.java
@@ -378,7 +378,6 @@ public final class QueryAssertions
         long start = System.nanoTime();
         log.info("Running import for %s", table.getObjectName());
         @Language("SQL") String sql = format("CREATE TABLE %s AS SELECT * FROM %s", table.getObjectName(), table);
-//        @Language("SQL") String sql = format("CREATE TABLE %s WITH (format='ORC',transactional=false, orc_bloom_filter_columns=ARRAY['orderkey'], orc_bloom_filter_fpp=0.001) AS SELECT * FROM %s", table.getObjectName(), table);
         long rows = (Long) queryRunner.execute(session, sql).getMaterializedRows().get(0).getField(0);
         log.info("Imported %s rows for %s in %s", rows, table.getObjectName(), nanosSince(start).convertToMostSuccinctTimeUnit());
     }
