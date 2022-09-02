f7ad1ebb10764a78101b700ad0d56b03104d23c1
Joseph Jacob <josephjacob1998@gmail.com>
2022-09-02 11:29:48 +0530

    implementing skeletal elastic search plan optimizer

---
+++ presto-elasticsearch/src/main/java/io/prestosql/elasticsearch/optimization/ElasticSearchPlanOptimizer.java
@@ -0,0 +1,30 @@
+package io.prestosql.elasticsearch.optimization;
+
+import com.google.inject.Inject;
+import io.prestosql.elasticsearch.ElasticsearchConfig;
+import io.prestosql.spi.ConnectorPlanOptimizer;
+import io.prestosql.spi.SymbolAllocator;
+import io.prestosql.spi.connector.ConnectorSession;
+import io.prestosql.spi.plan.PlanNode;
+import io.prestosql.spi.plan.PlanNodeIdAllocator;
+import io.prestosql.spi.type.Type;
+
+import java.util.Map;
+
+public class ElasticSearchPlanOptimizer implements ConnectorPlanOptimizer {
+
+    private ElasticsearchConfig elasticsearchConfig;
+
+    @Inject
+    public ElasticSearchPlanOptimizer(ElasticsearchConfig elasticsearchConfig) {
+        this.elasticsearchConfig = elasticsearchConfig;
+    }
+
+    @Override
+    public PlanNode optimize(PlanNode maxSubPlan, ConnectorSession session, Map<String, Type> types, SymbolAllocator symbolAllocator, PlanNodeIdAllocator idAllocator) {
+        if (!elasticsearchConfig.isPushDownEnabled()) {
+            return maxSubPlan;
+        }
+        return null;
+    }
+}
