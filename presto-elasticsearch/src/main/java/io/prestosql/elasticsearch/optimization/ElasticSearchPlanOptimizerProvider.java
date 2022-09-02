c9e74ce3b46342e78b4881d13e2a55a90bc3eae9
Joseph Jacob <josephjacob1998@gmail.com>
2022-09-02 11:40:48 +0530

    adding implementation for getLogicalplanoptimizer

--- presto-elasticsearch/src/main/java/io/prestosql/elasticsearch/optimization/ElasticSearchPlanOptimizerProvider.java
+++ presto-elasticsearch/src/main/java/io/prestosql/elasticsearch/optimization/ElasticSearchPlanOptimizerProvider.java
@@ -1,15 +1,24 @@
 package io.prestosql.elasticsearch.optimization;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.inject.Inject;
 import io.prestosql.spi.ConnectorPlanOptimizer;
 import io.prestosql.spi.connector.ConnectorPlanOptimizerProvider;
 
 import java.util.Set;
 
 public class ElasticSearchPlanOptimizerProvider implements ConnectorPlanOptimizerProvider {
+
+    private ElasticSearchPlanOptimizer elasticSearchPlanOptimizer;
+
+    @Inject
+    public ElasticSearchPlanOptimizerProvider(ElasticSearchPlanOptimizer elasticSearchPlanOptimizer) {
+        this.elasticSearchPlanOptimizer = elasticSearchPlanOptimizer;
+    }
+
     @Override
     public Set<ConnectorPlanOptimizer> getLogicalPlanOptimizers() {
-        return ImmutableSet.of();
+        return ImmutableSet.of(elasticSearchPlanOptimizer);
     }
 
     @Override
