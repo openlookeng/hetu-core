65a5b817f49199554543f54c03c2c43142bd3505
Joseph Jacob <josephjacob1998@gmail.com>
2022-09-02 14:55:22 +0530

    overriding visit plan in elasticsearchplanVisitor

--- presto-elasticsearch/src/main/java/io/prestosql/elasticsearch/optimization/ElasticsearchPlanVisitor.java
+++ presto-elasticsearch/src/main/java/io/prestosql/elasticsearch/optimization/ElasticsearchPlanVisitor.java
@@ -3,12 +3,14 @@ package io.prestosql.elasticsearch.optimization;
 import com.google.inject.Inject;
 import io.prestosql.spi.SymbolAllocator;
 import io.prestosql.spi.connector.ConnectorSession;
-import io.prestosql.spi.plan.PlanNode;
-import io.prestosql.spi.plan.PlanNodeIdAllocator;
-import io.prestosql.spi.plan.PlanVisitor;
+import io.prestosql.spi.plan.*;
 import io.prestosql.spi.type.Type;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
 public class ElasticsearchPlanVisitor extends PlanVisitor<PlanNode, Void> {
     private final PlanNodeIdAllocator idAllocator;
@@ -24,8 +26,32 @@ public class ElasticsearchPlanVisitor extends PlanVisitor<PlanNode, Void> {
         this.symbolAllocator = symbolAllocator;
     }
 
+    private static PlanNode replaceChildren(PlanNode node, List<PlanNode> children)
+    {
+        List<PlanNode> childrenNodes = node.getSources();
+        for (int i = 0; i < childrenNodes.size(); i++) {
+            if (children.get(i) != childrenNodes.get(i)) {
+                return node.replaceChildren(children);
+            }
+        }
+        return node;
+    }
+
+
     @Override
     public PlanNode visitPlan(PlanNode node, Void context) {
-        return null;
+        Optional<PlanNode> pushDownPlan = tryCreatingNewScanNode(node);
+        return pushDownPlan.orElseGet(() -> replaceChildren(
+                node, node.getSources().stream().map(source -> source.accept(this, null)).collect(toImmutableList())));
+    }
+
+    private Optional<PlanNode> tryCreatingNewScanNode(PlanNode node) {
+        if (node instanceof FilterNode) {
+            
+        } else if (node instanceof AggregationNode) {
+            // TODO: 9/2/2022 implement specific logic when developing pushdown for aggregate
+            return Optional.empty();
+        }
+        return Optional.empty();
     }
 }
