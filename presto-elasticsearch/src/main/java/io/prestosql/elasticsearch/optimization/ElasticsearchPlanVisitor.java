167ac4d649d6c842ffb6b519c248ee27be53bda6
Joseph Jacob <josephjacob1998@gmail.com>
2022-09-06 14:52:11 +0530

    temp commit

--- presto-elasticsearch/src/main/java/io/prestosql/elasticsearch/optimization/ElasticsearchPlanVisitor.java
+++ presto-elasticsearch/src/main/java/io/prestosql/elasticsearch/optimization/ElasticsearchPlanVisitor.java
@@ -1,9 +1,13 @@
 package io.prestosql.elasticsearch.optimization;
 
 import com.google.inject.Inject;
+import io.prestosql.elasticsearch.ElasticsearchTableHandle;
 import io.prestosql.spi.SymbolAllocator;
 import io.prestosql.spi.connector.ConnectorSession;
+import io.prestosql.spi.connector.ConnectorTableHandle;
+import io.prestosql.spi.metadata.TableHandle;
 import io.prestosql.spi.plan.*;
+import io.prestosql.spi.relation.RowExpression;
 import io.prestosql.spi.type.Type;
 
 import java.util.List;
@@ -47,11 +51,29 @@ public class ElasticsearchPlanVisitor extends PlanVisitor<PlanNode, Void> {
 
     private Optional<PlanNode> tryCreatingNewScanNode(PlanNode node) {
         if (node instanceof FilterNode) {
-            
+            return tryCreatingNewFilterNode(((FilterNode) node));
         } else if (node instanceof AggregationNode) {
             // TODO: 9/2/2022 implement specific logic when developing pushdown for aggregate
             return Optional.empty();
         }
         return Optional.empty();
+    }
+
+    private Optional<PlanNode> tryCreatingNewFilterNode(FilterNode node) {
+        RowExpression predicate = node.getPredicate();
+        Optional<String> esQuery = convertPredicateToESQuery(predicate);
+        if (!esQuery.isPresent()) {
+            return Optional.empty();
+        }
+        return Optional.empty();
+        /*TableScanNode tableScanNodeOriginal = (TableScanNode) node.getSource();
+        TableHandle tableHandleOriginal = tableScanNodeOriginal.getTable();
+        ElasticsearchTableHandle connectorHandle = (ElasticsearchTableHandle)tableHandleOriginal.getConnectorHandle();
+        TableScanNode tableScanNodeNew = new TableScanNode(idAllocator.getNextId(), tableHandleOriginal, tableScanNodeOriginal.getOutputSymbols(), tableScanNodeOriginal.getAssignments(), tableScanNodeOriginal.getEnforcedConstraint(), Optional.of(predicate), tableScanNodeOriginal.getStrategy(), )
+        FilterNode filterNodeNew = new FilterNode(idAllocator.getNextId(), , predicate)*/
+    }
+
+    private Optional<String> convertPredicateToESQuery(RowExpression predicate) {
+        return null;
     }
 }
