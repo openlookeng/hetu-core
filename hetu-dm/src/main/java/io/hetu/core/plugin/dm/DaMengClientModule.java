c6f8e3a994ab9e838a4c35744c1af9128c62535a
everypaopao <13126797781@163.com>
2022-10-17 14:37:34 +0800

    add dameng connector plugin

---
+++ hetu-dm/src/main/java/io/hetu/core/plugin/dm/DaMengClientModule.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.hetu.core.plugin.dm;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.google.inject.Scopes;
+import com.google.inject.Singleton;
+import io.prestosql.plugin.jdbc.BaseJdbcConfig;
+import io.prestosql.plugin.jdbc.ConnectionFactory;
+import io.prestosql.plugin.jdbc.DriverConnectionFactory;
+import io.prestosql.plugin.jdbc.JdbcClient;
+import io.prestosql.spi.PrestoException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Driver;
+import java.util.Optional;
+import java.util.Properties;
+
+import static io.airlift.configuration.ConfigBinder.configBinder;
+import static io.prestosql.plugin.jdbc.DriverConnectionFactory.basicConnectionProperties;
+import static io.prestosql.plugin.jdbc.JdbcErrorCode.JDBC_ERROR;
+
+public class DaMengClientModule
+        implements Module
+{
+    /**
+     * getConnectionFactory
+     *
+     * @param config config
+     * @return ConnectionFactory
+     */
+    @Provides
+    @Singleton
+    public static ConnectionFactory getConnectionFactory(BaseJdbcConfig config, DaMengConfig dmConfig)
+    {
+        Properties connectionProperties = basicConnectionProperties(config);
+        Driver driver;
+        try {
+            driver = (Driver) Class.forName(DaMengConstants.DM_JDBC_DRIVER_CLASS_NAME).getConstructor(((Class<?>[]) null)).newInstance();
+        }
+        catch (InstantiationException | ClassNotFoundException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            throw new PrestoException(JDBC_ERROR, e);
+        }
+
+        return new DriverConnectionFactory(driver, config.getConnectionUrl(),
+                Optional.ofNullable(config.getUserCredentialName()),
+                Optional.ofNullable(config.getPasswordCredentialName()), connectionProperties);
+    }
+
+    @Override
+    public void configure(Binder binder)
+    {
+        binder.bind(JdbcClient.class).to(DaMengClient.class).in(Scopes.SINGLETON);
+        configBinder(binder).bindConfig(BaseJdbcConfig.class);
+        configBinder(binder).bindConfig(DaMengConfig.class);
+    }
+}
---
+++ hetu-dm/src/main/java/io/hetu/core/plugin/dm/DaMengConstants.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.hetu.core.plugin.dm;
+
+public class DaMengConstants
+{
+    /**
+     * dameng connector name
+     */
+    public static final String DM_CONNECTOR_NAME = "dameng";
+
+    /**
+     * "TABLE_NAME"
+     */
+    public static final String TABLE_NAME = "TABLE_NAME";
+
+    /**
+     * dameng jdbc driver class name
+     */
+    public static final String DM_JDBC_DRIVER_CLASS_NAME = "dm.jdbc.driver.DmDriver";
+
+    private DaMengConstants()
+    {}
+}
